022b6bb743060995ce79e8a6e226cf2c151ef1ae
Nathan <saludesnathaniel@gmail.com>
2025-01-30 06:55:24 +0800

    About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,30 @@
+import ContentWrapper from "@/components/ContentWrapper";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ClientList from "@/components/sections/about/ClientList";
+import Hero from "@/components/sections/about/Hero";
+import MemberCards from "@/components/sections/about/MemberCards";
+import StickySidebar from "@/components/StickySidebar";
+
 export default function AboutPage() {
-  return <></>;
+  return (
+    <>
+      <Header
+        theme="dark"
+        disableLogoBorder
+        className="absolute top-0 z-50 w-full border-b-0 bg-black/45 lg:static lg:z-0 lg:bg-black"
+      />
+      <main className="bg-black">
+        <ContentWrapper className="border-b-2 border-black">
+          <StickySidebar theme="dark" className="mt-32 border-r-0" />
+          <div className="basis-full space-y-10 overflow-hidden bg-simmer-white pb-20 lg:rounded-tl-[8rem]">
+            <Hero />
+            <MemberCards />
+            <ClientList />
+          </div>
+        </ContentWrapper>
+      </main>
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/sections/about/Hero.tsx
@@ -0,0 +1,81 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { FC, HTMLProps } from "react";
+
+import PEEKHERE from "@/assets/about/peek-here.svg";
+import ArrowDown from "@/components/icons/ArrowDown";
+
+const Hero: FC<HTMLProps<HTMLDivElement>> = ({ className }) => {
+  return (
+    <section
+      className={cn(
+        "flex min-h-dvh flex-col bg-[url(/images/dash-lines.svg)] bg-bottom bg-repeat-x lg:bg-none",
+        className
+      )}
+    >
+      <div className="relative aspect-square border-b-2 border-black sm:aspect-video lg:order-2 lg:h-dvh lg:border-y-2">
+        <Image
+          src="/images/img_placeholder.jpg"
+          alt=""
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="mx-auto grid max-w-[1920px] lg:order-1 lg:grid-cols-[1fr_40%] lg:gap-8 lg:px-16">
+        <div className="flex items-center justify-center px-5 py-5 lg:items-start lg:justify-start lg:px-0 lg:py-16">
+          <h1 className="max-w-[8ch] text-center font-adelle-mono text-6xl font-bold tracking-tighter min-[425px]:text-7xl sm:text-8xl lg:text-left lg:text-[clamp(1rem,9vw,200px)]">
+            CREATIVE CHEFS
+          </h1>
+        </div>
+        <div className="border-y-2 border-black px-8 py-8 lg:flex lg:flex-col lg:justify-between lg:gap-10 lg:border-y-0 lg:py-16">
+          <div className="hidden lg:flex lg:justify-end">
+            <Image
+              src={PEEKHERE}
+              alt="Peek here"
+              className="lg:w-[80%] lg:pt-2 xl:w-[67%] min-[1440px]:w-[60%] min-[1920px]:w-[58%]"
+            />
+          </div>
+          <div className="space-y-2">
+            <p className="font-bold lg:text-[clamp(1rem,1vw,20px)]">
+              SOMETHING ABOUT
+            </p>
+            <p className="text-pretty lg:text-lg lg:leading-snug xl:text-xl xl:leading-[clamp(1rem,1.8vw,40px)] min-[1440px]:text-[clamp(1rem,1.1vw,25px)]">
+              The Bourbon Bros combines premium quality with a fun and
+              approachable personality, making it an excellent choice for those
+              looking for a high-quality bourbon that doesn&apos;t take itself
+              too seriously.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container order-3 grid gap-8 border-black px-8 py-12 md:grid-cols-2 lg:bg-none lg:py-14">
+        <div className="relative aspect-square overflow-hidden rounded-2xl border-2 border-black md:rounded-3xl md:border-none">
+          <Image
+            src="/images/img_placeholder.jpg"
+            alt=""
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="flex flex-col gap-8">
+          <div className="order-1 flex items-end justify-between rounded-2xl bg-black px-5 py-5 md:order-2 md:max-h-max md:rounded-3xl xl:px-8 xl:py-8">
+            <span className="inline-block max-w-[11ch] font-adelle-mono-flex text-3xl leading-none text-simmer-white sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
+              BRANDING IS PROBLEM SOLVING
+            </span>
+            <ArrowDown className="fill-simmer-white xl:h-20 xl:w-8" />
+          </div>
+          <div className="relative order-2 aspect-square flex-1 overflow-hidden rounded-2xl border-2 border-black md:order-1 md:aspect-auto md:rounded-3xl md:border-none">
+            <Image
+              src="/images/img_placeholder.jpg"
+              alt=""
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
