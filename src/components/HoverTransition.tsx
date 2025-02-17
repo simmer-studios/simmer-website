d8a1ca726061b2e7778277598a09ade8b1677c30
nathan <saludesnathaniel@gmail.com>
2025-02-17 16:56:00 +0800

    Update homepage hero microinteraction animations

--- src/components/HoverTransition.tsx
+++ src/components/HoverTransition.tsx
@@ -1,34 +1,44 @@
 "use client";
 
 import { HTMLMotionProps, motion } from "motion/react";
-import { FC, PropsWithChildren, ReactNode, useEffect, useState } from "react";
+import {
+  FC,
+  HTMLProps,
+  PropsWithChildren,
+  ReactNode,
+  useEffect,
+  useState
+} from "react";
+
+import { cn } from "@/lib/utils";
 
 interface Props {
   transitionElement: ReactNode;
 }
 
-const HoverTransition: FC<PropsWithChildren<Props>> = ({
+const HoverTransition: FC<HTMLProps<HTMLDivElement> & Props> = ({
   transitionElement,
+  className,
   children
 }) => {
   const [hovered, setHovered] = useState<boolean>(false);
 
   return (
     <motion.div
-      className="relative overflow-hidden"
+      className={cn("relative overflow-y-hidden", className)}
       onHoverStart={() => setHovered(true)}
       onHoverEnd={() => setHovered(false)}
     >
       <motion.div
-        animate={{ y: hovered ? "-100%" : "0%" }}
-        transition={{ type: "tween" }}
+        animate={{ y: hovered ? "-100%" : "0%", opacity: hovered ? 0 : 1 }}
+        transition={{ type: "spring", duration: 0.13 }}
       >
         {children}
       </motion.div>
       <motion.div
         className="absolute h-full w-full"
-        animate={{ y: hovered ? "-100%" : "0%" }}
-        transition={{ type: "tween" }}
+        animate={{ y: hovered ? "-100%" : "0%", opacity: hovered ? 1 : 0 }}
+        transition={{ type: "spring", duration: 0.13 }}
       >
         {transitionElement}
       </motion.div>
