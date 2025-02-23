ebdbba4d3276ea68730bdce07a9ab8c75560e24c
NathanSaludes <saludesnathaniel@gmail.com>
2025-02-24 04:03:46 +0800

    misc updates

--- src/components/sections/home/EggSpawner.tsx
+++ src/components/sections/home/EggSpawner.tsx
@@ -3,6 +3,8 @@
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
+import HalfEgg from "@/components/icons/HalfEgg";
+
 const FallingEggs = dynamic(
   () => import("@/components/sections/home/FallingEggs"),
   { ssr: false }
@@ -11,7 +13,7 @@ const FallingEggs = dynamic(
 const EggSpawner = () => {
   return (
     <section className="relative flex min-h-dvh overflow-hidden bg-simmer-white">
-      <div className="container relative z-20 mx-auto content-center space-y-5 text-center lg:space-y-8 xl:space-y-10">
+      <div className="container relative z-10 mx-auto content-center space-y-5 text-center lg:space-y-8 xl:space-y-10">
         <div className="flex flex-wrap justify-center gap-4 font-adelle-mono text-5xl font-bold sm:text-6xl md:gap-8 md:text-7xl lg:gap-10 lg:text-8xl xl:gap-16 xl:text-9xl">
           <h2 className="px-5">
             HOW{" "}
