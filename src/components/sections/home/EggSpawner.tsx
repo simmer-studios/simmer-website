bc8e27347267e2201066da00a452df17e0c54796
Nathan <saludesnathaniel@gmail.com>
2025-02-24 05:31:06 +0800

    Falling Eggs (#28)
    * initial commit for falling eggs animation
    
    * fix code
    
    * add matter-wrap plugin for matter-js
    
    * update eslint config
    
    * misc updates
    
    * Create function for spawning new bodies in the world
    
    * Include egg png image for texture
    
    * Add textures and external control to Falling Eggs scene

--- src/components/sections/home/EggSpawner.tsx
+++ src/components/sections/home/EggSpawner.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
+
+import { FallingEggsRef } from "@/components/sections/home/FallingEggs";
 
 const FallingEggs = dynamic(
   () => import("@/components/sections/home/FallingEggs"),
@@ -9,9 +11,20 @@ const FallingEggs = dynamic(
 );
 
 const EggSpawner = () => {
+  const fallingEggsRef = useRef<FallingEggsRef>(null);
+
+  const handleClick = () => {
+    if (fallingEggsRef.current) {
+      fallingEggsRef.current.spawnEgg();
+    }
+  };
+
   return (
     <section className="relative flex min-h-dvh overflow-hidden bg-simmer-white">
-      <div className="container relative z-20 mx-auto content-center space-y-5 text-center lg:space-y-8 xl:space-y-10">
+      <Suspense fallback={null}>
+        <FallingEggs ref={fallingEggsRef} />
+      </Suspense>
+      <div className="container relative mx-auto content-center space-y-5 text-center lg:space-y-8 xl:space-y-10">
         <div className="flex flex-wrap justify-center gap-4 font-adelle-mono text-5xl font-bold sm:text-6xl md:gap-8 md:text-7xl lg:gap-10 lg:text-8xl xl:gap-16 xl:text-9xl">
           <h2 className="px-5">
             HOW{" "}
@@ -20,7 +33,10 @@ const EggSpawner = () => {
           </h2>
         </div>
         <div>
-          <button className="min-h-11 rounded-full border-2 border-black bg-simmer-white px-6 py-2 font-adelle-mono text-lg active:brightness-95">
+          <button
+            onClick={handleClick}
+            className="relative z-30 min-h-11 rounded-full border-2 border-black bg-simmer-white px-6 py-2 font-adelle-mono text-lg active:brightness-95"
+          >
             TAP FOR EGGS
           </button>
         </div>
--- src/components/sections/home/FallingEggs.tsx
+++ src/components/sections/home/FallingEggs.tsx
@@ -1,20 +1,75 @@
 "use client";
 
-import { Bodies, Composite, Engine, Render, Runner } from "matter-js";
-import { RefObject, useEffect, useRef, useState } from "react";
+import Matter, {
+  Bodies,
+  Body,
+  Composite,
+  Engine,
+  Mouse,
+  MouseConstraint,
+  Render,
+  Runner,
+  Vector,
+  World
+} from "matter-js";
+// eslint-disable
+// @ts-ignore-
+import MatterWrap from "matter-wrap"; // needs to disable eslint here because no declaration file is found for matter-wrap
+// eslint-enable
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 
-const FallingEggs = () => {
+const THICCNESS = 100;
+
+export type FallingEggsRef = {
+  spawnEgg: () => void;
+};
+
+const FallingEggs = forwardRef<FallingEggsRef>((props, ref) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const engineRef = useRef<Engine>(undefined);
   const compositeRef = useRef<Composite>(undefined);
   const runnerRef = useRef<Runner>(undefined);
   const renderRef = useRef<Render>(undefined);
+  const bodies = useRef<Record<string, Body>>(undefined);
 
+  Matter.use(MatterWrap);
+
+  const handleResize = () => {
+    if (containerRef.current && renderRef.current && bodies.current) {
+      const width = containerRef.current.clientWidth;
+      const height = containerRef.current.clientHeight;
+      /* reset the canvas size */
+      renderRef.current.canvas.width = width;
+      renderRef.current.canvas.height = height;
+
+      /* reset the position of the ground */
+      Body.setPosition(
+        bodies.current.ground,
+        Vector.create(width / 2, height + THICCNESS / 2)
+      );
+
+      Body.setPosition(
+        bodies.current.rightWall,
+        Vector.create(width + THICCNESS, height / 2)
+      );
+    }
+  };
+
+  /* scene resize */
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  /* scene setup */
   useEffect(() => {
     if (containerRef.current && !engineRef.current) {
-      const container = containerRef.current.getBoundingClientRect();
-
-      console.log(container.width);
+      const container = {
+        width: containerRef.current.clientWidth,
+        height: containerRef.current.clientHeight
+      };
 
       const engine = Engine.create();
       const world = engine.world;
@@ -33,18 +88,63 @@ const FallingEggs = () => {
       engineRef.current = engine;
       renderRef.current = render;
 
+      engine.gravity.scale = 0.00987;
+
       // create world elements
-      const box = Bodies.rectangle(container.width / 2, 0, 80, 80, {
-        frictionAir: 0.02,
-        restitution: 0.09
-      });
-      const ground = Bodies.rectangle(0, container.height + 2, 4000, 2, {
-        isStatic: true
-      });
+      const leftWall = Bodies.rectangle(
+        0 - THICCNESS,
+        container.height / 2,
+        THICCNESS,
+        container.height,
+        {
+          isStatic: true
+        }
+      );
+
+      const rightWall = Bodies.rectangle(
+        container.width + THICCNESS,
+        container.height / 2,
+        THICCNESS,
+        container.height,
+        {
+          isStatic: true
+        }
+      );
+
+      const ground = Bodies.rectangle(
+        container.width / 2,
+        container.height + THICCNESS / 2,
+        container.width,
+        THICCNESS,
+        {
+          isStatic: true
+        }
+      );
+
+      bodies.current = {
+        ground,
+        leftWall,
+        rightWall
+      };
 
       // add all of the bodies into the world
-      Composite.add(world, [box, ground]);
-      compositeRef.current = Composite.add(world, [box, ground]);
+      const composite = Composite.add(world, [ground, leftWall, rightWall]);
+      compositeRef.current = composite;
+
+      // add mouse control
+      const mouse = Mouse.create(render.canvas);
+      const mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
+          }
+        }
+      });
+
+      World.add(world, mouseConstraint);
+      render.mouse = mouse;
 
       // run the renderer
       Render.run(render);
@@ -58,12 +158,49 @@ const FallingEggs = () => {
     }
   }, [containerRef]);
 
+  const spawnEgg = () => {
+    if (containerRef.current && compositeRef.current && engineRef.current) {
+      const container = {
+        width: containerRef.current.clientWidth,
+        height: containerRef.current.clientHeight
+      };
+
+      const egg = Bodies.circle(Math.random() * container.width, 0, 60, {
+        angle: Math.random() * Math.PI,
+        friction: 0.5,
+        density: 0.1,
+        restitution: 1,
+        plugin: {
+          wrap: {
+            min: { x: 0, y: 0 },
+            max: { x: container.width, y: container.height }
+          }
+        },
+        render: {
+          sprite: {
+            texture: "images/img_egg.png",
+            xScale: 0.8,
+            yScale: 0.8
+          }
+        }
+      });
+
+      Composite.add(compositeRef.current, [egg]);
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    spawnEgg
+  }));
+
   return (
     <div
       ref={containerRef}
-      className="absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+      className="absolute bottom-0 left-0 right-0 top-0 z-20 h-full w-full"
     />
   );
-};
+});
+
+FallingEggs.displayName = "FallingEggs";
 
 export default FallingEggs;
