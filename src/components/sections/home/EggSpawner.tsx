f2266d08bffc041de822de017fa14d86929d2e97
NathanSaludes <saludesnathaniel@gmail.com>
2025-02-24 01:04:03 +0800

    initial commit for falling eggs animation

--- src/components/sections/home/EggSpawner.tsx
+++ src/components/sections/home/EggSpawner.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const FallingEggs = dynamic(
+  () => import("@/components/sections/home/FallingEggs"),
+  { ssr: false }
+);
+
 const EggSpawner = () => {
   return (
-    <section className="bg-simmer-white">
-      <div className="container mx-auto min-h-dvh content-center space-y-5 text-center lg:space-y-8 xl:space-y-10">
+    <section className="relative flex min-h-dvh overflow-hidden bg-simmer-white">
+      <div className="container relative z-20 mx-auto content-center space-y-5 text-center lg:space-y-8 xl:space-y-10">
         <div className="flex flex-wrap justify-center gap-4 font-adelle-mono text-5xl font-bold sm:text-6xl md:gap-8 md:text-7xl lg:gap-10 lg:text-8xl xl:gap-16 xl:text-9xl">
           <h2 className="px-5">
             HOW{" "}
@@ -15,6 +25,9 @@ const EggSpawner = () => {
           </button>
         </div>
       </div>
+      <Suspense fallback={null}>
+        <FallingEggs />
+      </Suspense>
     </section>
   );
 };
