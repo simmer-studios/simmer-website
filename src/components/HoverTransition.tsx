c1e3b142846811fe2789dd986e41238ef89e6cce
Mark Louie Espedido <espi@espidesigns.com>
2025-03-10 15:11:31 +0800

    added tap effects on hoverTransition in homepage hero

--- src/components/HoverTransition.tsx
+++ src/components/HoverTransition.tsx
@@ -21,23 +21,25 @@ const HoverTransition: FC<HTMLProps<HTMLDivElement> & Props> = ({
   className,
   children
 }) => {
-  const [hovered, setHovered] = useState<boolean>(false);
+  const [isActive, setIsActive] = useState<boolean>(false);
 
   return (
     <motion.div
-      className={cn("relative overflow-y-hidden", className)}
-      onHoverStart={() => setHovered(true)}
-      onHoverEnd={() => setHovered(false)}
+      className={cn("relative cursor-pointer overflow-y-hidden", className)}
+      onHoverStart={() => setIsActive(true)}
+      onHoverEnd={() => setIsActive(false)}
+      whileTap={{ scale: 0.98 }}
+      onClick={() => setIsActive(!isActive)}
     >
       <motion.div
-        animate={{ y: hovered ? "-100%" : "0%", opacity: hovered ? 0 : 1 }}
+        animate={{ y: isActive ? "-100%" : "0%", opacity: isActive ? 0 : 1 }}
         transition={{ type: "spring", duration: 0.13 }}
       >
         {children}
       </motion.div>
       <motion.div
         className="absolute h-full w-full"
-        animate={{ y: hovered ? "-100%" : "0%", opacity: hovered ? 1 : 0 }}
+        animate={{ y: isActive ? "-100%" : "0%", opacity: isActive ? 1 : 0 }}
         transition={{ type: "spring", duration: 0.13 }}
       >
         {transitionElement}
