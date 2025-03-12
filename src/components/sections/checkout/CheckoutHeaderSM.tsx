7f134e5805423d8de03881d53b4440aa34cc79ea
Mark Louie Espedido <espi@espidesigns.com>
2025-03-12 18:44:50 +0800

    adds animation when in view of the checkout page

--- src/components/sections/checkout/CheckoutHeaderSM.tsx
+++ src/components/sections/checkout/CheckoutHeaderSM.tsx
@@ -1,29 +1,49 @@
+"use client";
+
+import { HTMLMotionProps, motion } from "framer-motion";
 import Image from "next/image";
-import { FC, HTMLProps } from "react";
+import { FC } from "react";
 
 import START from "@/assets/checkout/sm_start.svg";
 import WORKWITHUS from "@/assets/checkout/sm_work-with-us.svg";
 import { cn } from "@/lib/utils";
 
-const CheckoutHeaderSM: FC<HTMLProps<HTMLDivElement>> = ({
+interface CheckoutHeaderSMProps extends HTMLMotionProps<"div"> {
+  className?: string;
+}
+
+const CheckoutHeaderSM: FC<CheckoutHeaderSMProps> = ({
   className,
   ...props
 }) => {
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
       className={cn(
         "flex divide-x-2 divide-simmer-white border-b-2 border-simmer-white bg-simmer-yellow",
         className
       )}
       {...props}
     >
-      <div className="px-5 py-5">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className="px-5 py-5"
+      >
         <Image src={START} alt="Start" />
-      </div>
-      <div className="px-5 py-5">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="px-5 py-5"
+      >
         <Image src={WORKWITHUS} alt="Work with us" />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
