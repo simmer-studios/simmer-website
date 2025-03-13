48504fac553f6962b805ebeb7afae0c05f96f5d3
Mark Louie Espedido <espi@espidesigns.com>
2025-03-13 17:21:57 +0800

    adds loading screen and success on checkout form popout

---
+++ src/app/(frontend)/checkout/layout.tsx
@@ -0,0 +1,14 @@
+export function generateMetadata() {
+  return {
+    title: "Checkout | Simmer Studios",
+    description: ""
+  };
+}
+
+export default function CheckoutLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
