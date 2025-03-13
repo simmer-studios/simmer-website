959367110b9e60a2162fbfd140eafded5bac842c
Mark Louie Espedido <espi@espidesigns.com>
2025-03-13 17:21:57 +0800

    adds loading screen and success on checkout form popout

--- src/app/(frontend)/checkout/page.tsx
+++ src/app/(frontend)/checkout/page.tsx
@@ -1,30 +1,49 @@
+"use client";
+
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
+
 import CheckoutForm from "@/components/CheckoutForm";
 import ContentWrapper from "@/components/ContentWrapper";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import CheckoutHeaderLG from "@/components/sections/checkout/CheckoutHeaderLG";
-import CheckoutHeaderSM from "@/components/sections/checkout/CheckoutHeaderSM";
+import CheckoutSuccess from "@/components/sections/checkout/CheckoutSuccess";
 import StickySidebar from "@/components/StickySidebar";
-
-export function generateMetadata() {
-  return {
-    title: "Checkout | Simmer Studios",
-    description: ""
-  };
-}
 
 /* CHECKOUT PAGE */
 export default function CheckoutPage() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const handleSubmit = async () => {
+    setIsSubmitting(true);
+    // Simulate form submission delay
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setIsSubmitting(false);
+    setIsSuccess(true);
+  };
+
   return (
     <>
       <Header theme="dark" disableLogoBorder />
       <main>
         <ContentWrapper className="bg-black">
           <StickySidebar theme="dark" className="border-t-0" />
-          <div className="basis-full">
-            <CheckoutHeaderSM className="flex lg:hidden" />
-            <CheckoutHeaderLG className="hidden lg:flex" />
-            <CheckoutForm />
+          <div className="basis-full" id="checkout-body">
+            <AnimatePresence mode="wait">
+              {!isSuccess ? (
+                <motion.div
+                  key="checkout-form"
+                  initial={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <CheckoutForm onSubmit={handleSubmit} />
+                </motion.div>
+              ) : (
+                <CheckoutSuccess key="success" />
+              )}
+            </AnimatePresence>
           </div>
         </ContentWrapper>
       </main>
