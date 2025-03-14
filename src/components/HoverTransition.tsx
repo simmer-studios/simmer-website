ab5f620a4baaf7751ebd216e0e0a85bb8cfe136e
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2025-03-15 00:05:26 +0800

    Resolve merge conflicts

--- src/components/HoverTransition.tsx
+++ src/components/HoverTransition.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "motion/react";
-import { FC, HTMLProps, ReactNode, useEffect, useState } from "react";
+import { FC, HTMLProps, memo, ReactNode, useEffect, useState } from "react";
 
 import { useAnimation } from "@/contexts/AnimationContext";
 import { cn } from "@/lib/utils";
@@ -11,60 +11,76 @@ interface Props {
   delay?: number;
 }
 
-const HoverTransition: FC<HTMLProps<HTMLDivElement> & Props> = ({
-  transitionElement,
-  className,
-  children,
-  delay = 0
-}) => {
-  const [isActive, setIsActive] = useState<boolean>(false);
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-  const { isPlaying } = useAnimation();
+const HoverTransition: FC<HTMLProps<HTMLDivElement> & Props> = memo(
+  ({ transitionElement, className, children, delay = 0 }) => {
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+    const { isPlaying } = useAnimation();
 
-  useEffect(() => {
-    setIsMobile("ontouchstart" in window || navigator.maxTouchPoints > 0);
-  }, []);
+    useEffect(() => {
+      setIsMobile("ontouchstart" in window || navigator.maxTouchPoints > 0);
+    }, []);
 
-  useEffect(() => {
-    if (isPlaying) {
-      const timer = setTimeout(() => {
-        setIsActive(true);
-      }, delay);
-      return () => clearTimeout(timer);
-    } else {
-      setIsActive(false);
-    }
-  }, [isPlaying, delay]);
+    useEffect(() => {
+      if (isPlaying) {
+        const timer = setTimeout(() => {
+          setIsActive(true);
+        }, delay);
+        return () => {
+          clearTimeout(timer);
+        };
+      } else {
+        setIsActive(false);
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isPlaying]);
 
-  const transitionDuration = isMobile ? 1 : 0.13;
+    const transitionDuration = isMobile ? 1 : 0.13;
 
-  return (
-    <motion.div
-      className={cn(
-        "relative transform-gpu cursor-pointer overflow-y-hidden will-change-transform",
-        className
-      )}
-      onHoverStart={() => !isPlaying && setIsActive(true)}
-      onHoverEnd={() => !isPlaying && setIsActive(false)}
-      whileTap={{ scale: 0.98 }}
-      onClick={() => !isPlaying && setIsActive(!isActive)}
-      style={{ backfaceVisibility: "hidden" }}
-    >
+    return (
       <motion.div
-        animate={{ y: isActive ? "-100%" : "0%", opacity: isActive ? 0 : 1 }}
-        transition={{ type: "spring", duration: transitionDuration }}
+        className={cn(
+          "relative transform-gpu cursor-pointer overflow-y-hidden will-change-transform",
+          className
+        )}
+        onHoverStart={() => !isPlaying && setIsActive(true)}
+        onHoverEnd={() => !isPlaying && setIsActive(false)}
+        whileTap={{ scale: 0.98 }}
+        onClick={() => !isPlaying && setIsActive(!isActive)}
+        style={{ backfaceVisibility: "hidden" }}
       >
-        {children}
+        <motion.div
+          className={cn("relative cursor-pointer overflow-y-hidden", className)}
+          onHoverStart={() => !isPlaying && setIsActive(true)}
+          onHoverEnd={() => !isPlaying && setIsActive(false)}
+          whileTap={{ scale: 0.98 }}
+          onClick={() => !isPlaying && setIsActive(!isActive)}
+        >
+          <motion.div
+            animate={{
+              y: isActive ? "-100%" : "0%",
+              opacity: isActive ? 0 : 1
+            }}
+            transition={{ type: "spring", duration: transitionDuration }}
+          >
+            {children}
+          </motion.div>
+          <motion.div
+            className="absolute h-full w-full"
+            animate={{
+              y: isActive ? "-100%" : "0%",
+              opacity: isActive ? 1 : 0
+            }}
+            transition={{ type: "spring", duration: transitionDuration }}
+          >
+            {transitionElement}
+          </motion.div>
+        </motion.div>
       </motion.div>
-      <motion.div
-        className="absolute h-full w-full"
-        animate={{ y: isActive ? "-100%" : "0%", opacity: isActive ? 1 : 0 }}
-        transition={{ type: "spring", duration: transitionDuration }}
-      >
-        {transitionElement}
-      </motion.div>
-    </motion.div>
-  );
-};
+    );
+  }
+);
+
+HoverTransition.displayName = "HoverTransition";
 
 export default HoverTransition;
