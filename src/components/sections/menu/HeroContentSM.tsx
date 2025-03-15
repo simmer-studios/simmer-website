4c630abd3d2b4b8964dd4fcf1703314f13399cf9
Mark Louie Espedido <espi@espidesigns.com>
2025-03-15 12:50:15 +0800

    adds simple page animation in view on menu page

--- src/components/sections/menu/HeroContentSM.tsx
+++ src/components/sections/menu/HeroContentSM.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import { HTMLMotionProps, motion } from "motion/react";
 import Image from "next/image";
-import { FC, HTMLProps } from "react";
+import { FC } from "react";
 
 import AREYOU from "@/assets/menu/sm_areyou.svg";
 import CRAVING from "@/assets/menu/sm_craving.svg";
@@ -9,56 +12,126 @@ import QUESTIONMARK from "@/assets/menu/sm_question-mark.svg";
 import WHAT from "@/assets/menu/sm_what.svg";
 import { cn } from "@/lib/utils";
 
-const HeroContentSM: FC<HTMLProps<HTMLDivElement>> = ({
-  className,
-  ...props
-}) => {
+const HeroContentSM: FC<HTMLMotionProps<"div">> = ({ className, ...props }) => {
   return (
-    <div className={cn("divide-y-2 divide-simmer-white", className)} {...props}>
-      <div className="row">
+    <motion.div
+      className={cn("divide-y-2 divide-simmer-white", className)}
+      {...props}
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: {},
+        visible: {
+          transition: {
+            staggerChildren: 0.1
+          }
+        }
+      }}
+    >
+      <motion.div
+        className="row"
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 }
+        }}
+      >
         <div className="container">
           <div className="flex divide-x-2 divide-simmer-white">
-            <div className="px-8 py-8">
+            <motion.div
+              className="px-8 py-8"
+              variants={{
+                hidden: { opacity: 0, x: -20 },
+                visible: { opacity: 1, x: 0 }
+              }}
+            >
               <Image src={WHAT} alt="What" />
-            </div>
+            </motion.div>
             <div className="flex-1"></div>
           </div>
         </div>
-      </div>
-      <div className="row">
+      </motion.div>
+      <motion.div
+        className="row"
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 }
+        }}
+      >
         <div className="container">
           <div className="flex divide-x-2 divide-simmer-white">
-            <div className="flex flex-1 items-center justify-center px-8 py-8">
+            <motion.div
+              className="flex flex-1 items-center justify-center px-8 py-8"
+              variants={{
+                hidden: { opacity: 0, x: -20 },
+                visible: { opacity: 1, x: 0 }
+              }}
+            >
               <Image src={AREYOU} alt="Are You" />
-            </div>
+            </motion.div>
           </div>
         </div>
-      </div>
-      <div className="row">
+      </motion.div>
+      <motion.div
+        className="row"
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 }
+        }}
+      >
         <div className="container">
           <div className="flex">
-            <div className="flex items-center justify-center px-8 py-8">
+            <motion.div
+              className="flex items-center justify-center px-8 py-8"
+              variants={{
+                hidden: { opacity: 0, x: -20 },
+                visible: { opacity: 1, x: 0 }
+              }}
+            >
               <Image src={CRAVING} alt="Craving" />
-            </div>
+            </motion.div>
           </div>
         </div>
-      </div>
-      <div className="row">
+      </motion.div>
+      <motion.div
+        className="row"
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 }
+        }}
+      >
         <div className="container">
           <div className="flex divide-x-2 divide-simmer-white">
-            <div className="flex w-[15%] items-end justify-center py-8">
+            <motion.div
+              className="flex w-[15%] items-end justify-center py-8"
+              variants={{
+                hidden: { opacity: 0, x: -20 },
+                visible: { opacity: 1, x: 0 }
+              }}
+            >
               <Image src={MENU} alt="Menu" />
-            </div>
-            <div className="flex flex-1 items-center px-8 py-8">
+            </motion.div>
+            <motion.div
+              className="flex flex-1 items-center px-8 py-8"
+              variants={{
+                hidden: { opacity: 0 },
+                visible: { opacity: 1 }
+              }}
+            >
               <Image src={FOR} alt="For" />
-            </div>
-            <div className="flex w-[20%] items-end justify-center py-8">
+            </motion.div>
+            <motion.div
+              className="flex w-[20%] items-end justify-center py-8"
+              variants={{
+                hidden: { opacity: 0, x: 20 },
+                visible: { opacity: 1, x: 0 }
+              }}
+            >
               <Image src={QUESTIONMARK} alt="?" />
-            </div>
+            </motion.div>
           </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
