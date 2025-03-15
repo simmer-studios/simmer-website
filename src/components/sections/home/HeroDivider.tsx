e201b3ce7b7fcd3ec3ffea747f50dd7d0eb57aaa
NathanSaludes <saludesnathaniel@gmail.com>
2025-03-15 14:23:40 +0800

    Remove visible line under divider by moving downward 1px.

--- src/components/sections/home/HeroDivider.tsx
+++ src/components/sections/home/HeroDivider.tsx
@@ -1,7 +1,7 @@
 const HeroDivider = () => {
   return (
     <div className="relative flex">
-      <div className="absolute bottom-0 flex min-h-[40vw] w-full bg-[url(/images/img_hero-divider-tablet.svg)] bg-cover bg-bottom bg-no-repeat lg:min-h-[20vw] lg:bg-[url(/images/img_hero-divider.svg)]"></div>
+      <div className="absolute bottom-0 flex min-h-[40vw] w-full translate-y-1 bg-[url(/images/img_hero-divider-tablet.svg)] bg-cover bg-bottom bg-no-repeat lg:min-h-[20vw] lg:bg-[url(/images/img_hero-divider.svg)]"></div>
     </div>
   );
 };
