0c49c1595d2b4399419aeec34d3a53ad2f9a1d5e
NathanSaludes <saludesnathaniel@gmail.com>
2025-03-17 02:56:05 +0800

    Fix IconHeadingCaptionCombo block

--- src/components/IconHeadingCaptionCombo.tsx
+++ src/components/IconHeadingCaptionCombo.tsx
@@ -24,16 +24,16 @@ const IconHeadingCaptionCombo: FC<HTMLProps<HTMLDivElement> & Props> = ({
   return (
     <section className={cn("w-full px-10", className)} {...props}>
       <div className="container flex flex-col gap-4 font-articulat lg:flex-row lg:gap-10">
-        <div className="flex-shrink-0">
+        <div className="flex h-[32px] w-[32px] flex-shrink-0 items-start justify-start lg:h-[40px]">
           <Image
             src={icon.url}
             alt={icon.alt || ""}
             width={32.93}
             height={82.05}
-            className="w-20 lg:w-24"
+            className="h-full w-full object-scale-down"
           />
         </div>
-        <h2 className="text-pretty text-4xl font-bold tracking-tight lg:basis-full lg:text-6xl">
+        <h2 className="w-full text-pretty text-4xl font-bold tracking-tight lg:min-w-[5ch] lg:max-w-[20ch] lg:text-6xl">
           {heading}
         </h2>
         <p className="basis-full text-lg">{caption}</p>
