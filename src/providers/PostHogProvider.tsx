4847dff0123fc6b35a16b35902742a1e8e723aa8
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2025-03-19 00:05:19 +0800

    Capture page leaves

--- src/providers/PostHogProvider.tsx
+++ src/providers/PostHogProvider.tsx
@@ -11,7 +11,8 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       api_host:
         process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
       person_profiles: "always", // create profiles for anonymous users as well
-      capture_pageview: false // Disable automatic pageview capture, as we capture manually
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually,
+      capture_pageleave: true // Capture page leave events
     });
   }, []);
 
