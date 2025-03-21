4c18a611c51b1ef8aff183b07c96017de7079517
Mark Louie Espedido <espi@espidesigns.com>
2025-03-21 08:03:14 +0800

    Works fix (#67)
    * fixes white borders showing on hover in portfolio grid
    
    * fixes paint flashing in works page
    
    * updates title in portfolio grid on mobile

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -26,17 +26,17 @@ const PortfolioItem: React.FC<Props> = ({ slug, name, category, image }) => {
   return (
     <Link
       href={`/works/${slug}`}
-      className="group relative block aspect-square overflow-hidden bg-gray-100 transition duration-300 ease-in-out"
+      className="group relative block aspect-square overflow-hidden bg-black transition duration-300 ease-in-out"
       scroll={true}
     >
       <CMSMedia
-        className="absolute h-full w-full object-cover transition-all duration-300 ease-in-out group-hover:scale-110 group-hover:brightness-90"
+        className="will-change-scale absolute h-full w-full transform-gpu object-cover transition-all duration-300 ease-in-out will-change-transform group-hover:scale-110 group-hover:opacity-90"
         controls={false}
         media={image}
       />
       <div
         ref={magneticRef}
-        className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-[3px] border-black bg-simmer-white px-14 py-6 text-center transition-all duration-300 ease-out will-change-transform md:invisible md:opacity-0 md:group-hover:visible md:group-hover:opacity-100"
+        className="will-change-opacity pointer-events-none absolute bottom-0 left-0 right-0 top-auto translate-x-0 translate-y-0 transform-gpu border-t-[2px] border-black bg-simmer-white px-14 py-6 text-center transition-all duration-300 ease-out will-change-transform md:invisible md:bottom-auto md:left-1/2 md:right-auto md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:border-[3px] md:opacity-0 md:group-hover:visible md:group-hover:opacity-100"
       >
         <span className="pointer-events-none block font-adelle-mono text-2xl font-bold uppercase tracking-tight text-black">
           {name}
