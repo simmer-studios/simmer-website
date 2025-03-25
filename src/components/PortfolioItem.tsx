998acad7c72ae32e5d4fd688e884cb4e9a7a31b7
Mark Louie Espedido <espi@espidesigns.com>
2025-03-25 09:41:42 +0800

    fixes title position in portfolio items on mobile

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
 import { useMagneticHover } from "@/hooks/useMagneticHover";
 import { isValidImage } from "@/lib/utils";
@@ -18,6 +19,15 @@ interface Props {
 
 const PortfolioItem: React.FC<Props> = ({ slug, name, category, image }) => {
   const magneticRef = useMagneticHover(100);
+
+  useEffect(() => {
+    // Reset transform when component unmounts or when screen size/hover support changes
+    return () => {
+      if (magneticRef.current) {
+        magneticRef.current.style.transform = "translate(-50%, -50%)";
+      }
+    };
+  }, []);
 
   if (typeof image === "number") {
     return null;
@@ -36,7 +46,7 @@ const PortfolioItem: React.FC<Props> = ({ slug, name, category, image }) => {
       />
       <div
         ref={magneticRef}
-        className="will-change-opacity pointer-events-none absolute bottom-0 left-0 right-0 top-auto translate-x-0 translate-y-0 transform-gpu border-t-[2px] border-black bg-simmer-white px-14 py-6 text-center transition-all duration-300 ease-out will-change-transform md:invisible md:bottom-auto md:left-1/2 md:right-auto md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:border-[3px] md:opacity-0 md:group-hover:visible md:group-hover:opacity-100"
+        className="will-change-opacity pointer-events-none absolute bottom-0 left-0 right-0 top-auto transform-gpu border-t-[2px] border-black bg-simmer-white px-14 py-6 text-center transition-all duration-300 ease-out will-change-transform md:invisible md:bottom-auto md:left-1/2 md:right-auto md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:border-[3px] md:opacity-0 md:group-hover:visible md:group-hover:opacity-100"
       >
         <span className="pointer-events-none block font-adelle-mono text-2xl font-bold uppercase tracking-tight text-black">
           {name}
